a3f92947f55bfc8d66e25683d6552fc26543319e
HellBaron <1146528091@qq.com>
2022-09-15 20:04:16 +0800

    更新代码

---
+++ UGUIProject/Assets/Scripts/ImageClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ImageClick : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("ImageClient OnPointerClick Call");
+    }
+}
